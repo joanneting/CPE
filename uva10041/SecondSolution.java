5b681c3c5e2738b2a3e662abc178bae4fede756a
Unknown <j87101488@gmail.com>
2019-09-30 17:35:11 +0800

    uva10041

---
+++ uva10041/SecondSolution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int caseNumber=input.nextInt();
+    int[] peopleArray;
+  
+    for(int i=0;i<caseNumber;i++){
+      int people=input.nextInt();
+      peopleArray=new int[people];
+       int total=0;
+      for(int j=0;j<people;j++){
+        peopleArray[j]=input.nextInt();
+      }
+      int vito=0;
+      Arrays.sort(peopleArray);     
+      vito=peopleArray[people/2];
+      for(int j=0;j<people;j++){
+        total+=Math.abs(vito-peopleArray[j]);
+      }
+      System.out.println(total);
+    }
+  }
+}
