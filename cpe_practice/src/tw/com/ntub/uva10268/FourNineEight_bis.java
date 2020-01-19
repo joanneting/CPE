8bdbf73975d46be02551a464653e7d8bcb996ed3
JoanneLin <j87101488@gmail.com>
2020-01-19 14:54:09 +0800

    uva10268

--- cpe_practice/src/tw/com/ntub/uva10268/FourNineEight_bis.java
+++ cpe_practice/src/tw/com/ntub/uva10268/FourNineEight_bis.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class FourNineEight_bis {
 	public static void main(String[] arg) {
 		Scanner input = new Scanner(System.in);
-		
-		while(input.hasNext()) {
+		while(input.hasNextLine()) {
 			String line = input.nextLine().trim();
 			long xValue = Long.valueOf(line);
-			line = input.nextLine().replaceAll("  "," ");
-			String[] finalArray = line.split(" ");
+			line = input.nextLine();
+			String[] finalArray = line.split("\\s+");
 			long result=0;
 			int nValue = finalArray.length-1;
-//			System.out.println(finalArray.length);
-			for(int i=0;i<=nValue;i++) {
-//				System.out.println("a  :  " + finalArray[i]);
-//				System.out.println(xValue+"n : "+(nValue-(i+1)));
-//				System.out.println("result  :  " + Math.pow(xValue,nValue-(i+1)));
-				if(xValue==0 && (nValue-(i+1))==-1)
-					continue;
-//				System.out.println("result  :  " + Math.pow(xValue,nValue-(i+1)));
-				result += Long.valueOf(finalArray[i])*(nValue-i)*Math.pow(xValue,nValue-(i+1));
-//				System.out.println(result);
+			/*cpe評測可過，uva不行
+			for(int i=0;i<nValue;i++) {
+				result += Long.valueOf(finalArray[i])*(nValue-i)*Math.pow(xValue,nValue-(i+1));//Math.pow() return int ，long 會有溢出問題
 			}
+			*/
+			
+			long xp = 1;
+			for(int i = nValue-1;i>=0;i--) {	
+				result +=  Long.valueOf(finalArray[i])*(nValue-i)*xp;
+				xp*=xValue;
+			}
+			
 			System.out.println(result);
 			
 		}
