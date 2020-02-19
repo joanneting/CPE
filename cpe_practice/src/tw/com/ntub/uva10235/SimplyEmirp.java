bb120e165834b381cce99f1b40ac0164e7778685
JoanneLin <j87101488@gmail.com>
2020-02-19 23:04:28 +0800

    way2

--- cpe_practice/src/tw/com/ntub/uva10235/SimplyEmirp.java
+++ cpe_practice/src/tw/com/ntub/uva10235/SimplyEmirp.java
@@ -1,51 +1,60 @@
 package tw.com.ntub.uva10235;
+
 import java.util.Scanner;
+
 public class SimplyEmirp {
 	public static void main(String arg[]) {
 		Scanner input = new Scanner(System.in);
-		while(input.hasNext()) {
+		int count = 0;
+		while (input.hasNext() && count < 1000000) {
 			int number = input.nextInt();
 			boolean prime = true;
 			boolean emirp = true;
-			for(int i = 2; i < number; i++) {
-				
-				if(number/i == number/Double.valueOf(i) || number/(number-i) == number/Double.valueOf(number-i)) {
-//					System.out.println(number / i + " "+number/(number-i));
-					if(number -i!=1) {
-						prime = false;
-						break;
-					}
-				}
-				
-				if(i == number-(i-2) || i == number-(i-2)+1) {
-					break;
-				}
-			}
+			prime = judge(number);
 			String numberStr = String.valueOf(number);
 			String rebmunStr = "";
-			for(int i = numberStr.length()-1; i >= 0 ; i--) {
-				rebmunStr +=numberStr.charAt(i);
+			for (int i = numberStr.length() - 1; i >= 0; i--) {
+				rebmunStr += numberStr.charAt(i);
 			}
 			int rebmun = Integer.parseInt(rebmunStr);
-			if(number == rebmun) emirp = false;
-			for (int i = 2; i < rebmun; i++) {
-				if(!emirp) break;
-				if (rebmun / i == rebmun / Double.valueOf(i) || rebmun/(rebmun-i) == rebmun/Double.valueOf(rebmun-i)) {
-					emirp = false;
-				}
+			if (number == rebmun) {
+				emirp = false;
+			}else {
+				emirp = judge(rebmun);
+			}
+
+			if (prime && !emirp) {
+				System.out.println(number + " is prime.");
+			} else if (prime && emirp) {
+				System.out.println(number + " is emirp.");
+			} else {
+				System.out.println(number + " is not prime.");
+			}
+			count++;
+		}
+	}
+
+	public static boolean judge(int number) {
+		for (int i = 2; i <= number/2; i++) {
 			
-				if(i == rebmun-(i-2) || i == rebmun-(i-2)+1) {
-					break;
+			if (number % i ==0) {
+				
+					return false;
+				
+			}
+			if(number/2-i==0) {
+				break;
+			}
+			if(number%(number/2-i)==0 ) {
+				if (number/2 - i != 1) {
+					return false;
 				}
 			}
-			
-			if(prime && !emirp) {
-				System.out.println(number+" is prime.");
-			}else if(prime && emirp) {
-				System.out.println(number+" is emirp.");
-			}else{
-				System.out.println(number+" is not prime.");
+
+			if (i == number/2 - (i - 2) || i == number/2 - (i - 2) + 1) {
+				break;
 			}
 		}
+		return true;
 	}
 }
