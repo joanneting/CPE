2c5332db0b9110f6c08b512d82e5fa8a23682091
JoanneLin <j87101488@gmail.com>
2020-02-19 23:10:28 +0800

    uva10235 Accepted

--- cpe_practice/src/tw/com/ntub/uva10235/SimplyEmirp.java
+++ cpe_practice/src/tw/com/ntub/uva10235/SimplyEmirp.java
@@ -1,60 +1,46 @@
-package tw.com.ntub.uva10235;
-
-import java.util.Scanner;
-
-public class SimplyEmirp {
-	public static void main(String arg[]) {
-		Scanner input = new Scanner(System.in);
-		int count = 0;
-		while (input.hasNext() && count < 1000000) {
-			int number = input.nextInt();
-			boolean prime = true;
-			boolean emirp = true;
+package tw.com.ntub.uva10235; 
+import java.util.Scanner; 
+public class SimplyEmirp { 
+	public static void main(String arg[]) { 
+		Scanner input = new Scanner(System.in); 
+		while(input.hasNext()) { 
+			int number = input.nextInt(); 
+			boolean prime = true; 
+			boolean emirp = true; 
 			prime = judge(number);
-			String numberStr = String.valueOf(number);
-			String rebmunStr = "";
-			for (int i = numberStr.length() - 1; i >= 0; i--) {
-				rebmunStr += numberStr.charAt(i);
-			}
+			String numberStr = String.valueOf(number); 
+			String rebmunStr = ""; 
+			for(int i = numberStr.length()-1; i >= 0 ; i--) { 
+				rebmunStr +=numberStr.charAt(i); 
+			} 
 			int rebmun = Integer.parseInt(rebmunStr);
-			if (number == rebmun) {
-				emirp = false;
+			if(number == rebmun) {
+				emirp = false; 
 			}else {
 				emirp = judge(rebmun);
 			}
-
-			if (prime && !emirp) {
-				System.out.println(number + " is prime.");
-			} else if (prime && emirp) {
-				System.out.println(number + " is emirp.");
-			} else {
-				System.out.println(number + " is not prime.");
-			}
-			count++;
-		}
-	}
-
+			 
+			if(prime && !emirp) { 
+				System.out.println(number+" is prime."); 
+			}else if(prime && emirp) { 
+				System.out.println(number+" is emirp."); 
+			}else{ 
+				System.out.println(number+" is not prime."); 
+			} 
+		} 
+	} 
 	public static boolean judge(int number) {
-		for (int i = 2; i <= number/2; i++) {
-			
-			if (number % i ==0) {
-				
-					return false;
-				
-			}
-			if(number/2-i==0) {
-				break;
-			}
-			if(number%(number/2-i)==0 ) {
-				if (number/2 - i != 1) {
-					return false;
-				}
-			}
-
-			if (i == number/2 - (i - 2) || i == number/2 - (i - 2) + 1) {
-				break;
-			}
-		}
+		for(int i = 2; i < number; i++) { 
+			if(number%i == 0|| number%(number-i) == 0) { 
+				if(number -i!=1) { 
+					return false; 
+				} 
+			} 
+			 
+			if(i == number-(i-2) || i == number-(i-2)+1) { 
+				break; 
+			} 
+		} 
 		return true;
 	}
-}
+} 
